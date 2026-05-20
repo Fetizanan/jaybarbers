a43151ad00c1b4af1c74149f828aa3d9ae4efc51
Fetizanan <127284216+Fetizanan@users.noreply.github.com>
2026-05-21 07:43:49 +0800

    Add Color theme

---
+++ android/app/src/main/kotlin/com/jaybarbers/app/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.jaybarbers.app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PrimaryLight = Color(0xFF1F1F1F)
+val PrimaryDark = Color(0xFFFFD700)
+val SecondaryLight = Color(0xFFD4AF37)
+val SecondaryDark = Color(0xFF1F1F1F)
+val TertiaryLight = Color(0xFF6200EE)
+val TertiaryDark = Color(0xFF03DAC6)
+val BackgroundLight = Color(0xFFFAFAFA)
+val BackgroundDark = Color(0xFF121212)
+val SurfaceLight = Color(0xFFFFFFFF)
+val SurfaceDark = Color(0xFF1E1E1E)
+val AccentGold = Color(0xFFD4AF37)
+val AccentRed = Color(0xFFE74C3C)
+val AccentGreen = Color(0xFF27AE60)
